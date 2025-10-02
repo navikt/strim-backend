b8260479ca92566bd7c0045dbb2c3de9f8f3cffb
Haimanot Tekie <haimanot.tekie@nav.no>
2025-10-02 15:59:23 +0200

    enable db

--- src/main/kotlin/strim/EventController.kt
+++ src/main/kotlin/strim/EventController.kt
@@ -4,10 +4,8 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/events")
-//TODO enable after db has been provisioned
-//class EventController(private val eventRepository: EventRepository) {
-class EventController() {
+class EventController(private val eventRepository: EventRepository) {
 
     @GetMapping
-    fun getAllEvents(): List<Event> = emptyList()
+    fun getAllEvents(): List<Event> = eventRepository.findAll()
 }
