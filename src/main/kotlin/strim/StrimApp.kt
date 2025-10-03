65c70332e1226c89a20f62168523818742dec523
Haimanot Tekie <haimanot.tekie@nav.no>
2025-10-03 12:28:35 +0200

    include db config

--- src/main/kotlin/strim/StrimApp.kt
+++ src/main/kotlin/strim/StrimApp.kt
@@ -1,18 +1,9 @@
 package strim
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [
-        DataSourceAutoConfiguration::class,
-        HibernateJpaAutoConfiguration::class,
-        FlywayAutoConfiguration::class
-    ]
-)
+@SpringBootApplication
 class StrimApp
 
 fun main(args: Array<String>) {
