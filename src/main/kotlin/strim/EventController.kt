4b878519c4df1829e9374ddc72b680f42ea948b1
Henrik uran <henrik.uran.nykmark@nav.no>
2025-12-23 14:00:49 +0100

    lagt til nytt endepunkt for møte detalje side

--- src/main/kotlin/strim/EventController.kt
+++ src/main/kotlin/strim/EventController.kt
@@ -2,12 +2,16 @@ package strim
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import strim.validering.EventValidator
 import java.time.LocalDateTime
+import java.util.UUID
 
 @RestController
 @RequestMapping("/events")
@@ -35,6 +39,15 @@ class EventController(private val eventRepository: EventRepository) {
         logger.info("Fetched {} past events", pastEvents.size)
         logger.debug("Past events: {}", pastEvents)
         return pastEvents
+    }
+    @GetMapping("/{id}")
+    fun getEventById(@PathVariable id: UUID): Event {
+        logger.info("fikk forespørsel om å hente event med id {}", id)
+
+        return eventRepository.findById(id)
+            .orElseThrow {
+                ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found")
+            }
     }
 
     @PostMapping("/create")
