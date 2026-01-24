48a81d266b73e7baa4020ffc9c28c6ff4bc2f0f9
Henrik uran <henrik.uran.nykmark@nav.no>
2026-01-24 02:29:24 +0100

    prøver gamle SecurityConfig.kt

--- src/main/kotlin/strim/SecurityConfig.kt
+++ src/main/kotlin/strim/SecurityConfig.kt
@@ -1,52 +1,24 @@
 package strim
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
-
     @Bean
-    @Profile("!local")
-    fun jwtDecoder(
-        @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") jwkSetUri: String
-    ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build()
-
-    @Bean
-    @Profile("local")
-    fun localFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf { it.disable() }
-            .authorizeHttpRequests {
-                it.anyRequest().permitAll()
-            }
-        return http.build()
-    }
-
-    @Bean
-    @Profile("!local")
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf { it.disable() }
+        http.csrf { it.disable() }
             .authorizeHttpRequests {
-                it.requestMatchers("/internal/**").permitAll()
+                it.requestMatchers("/internal/actuator/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/events/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/events/*/join").authenticated()
-                    .requestMatchers(HttpMethod.DELETE, "/events/*/join").authenticated()
-                    .requestMatchers(HttpMethod.GET, "/me").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/events/create").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/categories").permitAll()
+                    .requestMatchers("/internal/**").permitAll()
                     .anyRequest().denyAll()
             }
-            .oauth2ResourceServer { rs ->
-                rs.jwt { }
-            }
-
         return http.build()
     }
 }
