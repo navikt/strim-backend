4dc5c31ba7455e203af624b6319d65d58e6052f4
Henrik uran <henrik.uran.nykmark@nav.no>
2026-01-24 02:19:32 +0100

    fix?2

--- src/main/kotlin/strim/SecurityConfig.kt
+++ src/main/kotlin/strim/SecurityConfig.kt
@@ -1,14 +1,23 @@
 package strim
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
+
+    @Bean
+    @Profile("!local")
+    fun jwtDecoder(
+        @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") jwkSetUri: String
+    ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build()
 
     @Bean
     @Profile("local")
